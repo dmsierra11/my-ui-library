72ce63822c4c75160b44ce8de7b9b5c68720c389
Daniel Sierra <danielsierra@Setup-User.local>
2024-05-18 18:25:14 +0200

    Moved button to components folder

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,5 +1,5 @@
 import { fn } from '@storybook/test';
-import { Button } from '../components/Button';
+import { Button } from '../components/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
